132a877c248225c8418a90040543e235658702cf
蒋勇 <jiangy@highzap.com>
2021-09-06 15:00:51 +0800

    feat:增加TaskList.vue及Story

---
+++ src/stories/Tasklist.stories.js
@@ -0,0 +1,64 @@
+import TaskList from '../components/TaskList.vue';
+
+import * as TaskStories from './Task.stories';
+
+export default {
+  component: TaskList,
+  title: 'TaskList',
+  decorators: [() => ({ template: '<div style="margin: 3em;"><story/></div>' })],
+  argTypes: {
+    onPinTask: {},
+    onArchiveTask: {},
+  },
+};
+
+// 模板函数
+const Template = args => ({
+  components: { TaskList },
+  setup() {
+    return { args, ...TaskStories.actionsData };
+  },
+  template: '<TaskList v-bind="args" />',
+});
+
+// 状态 1：默认
+export const Default = Template.bind({});
+Default.args = {
+  // Shaping the stories through args composition.
+  // The data was inherited from the Default story in task.stories.js.
+  tasks: [
+    { ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
+    { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
+    { ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
+    { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
+    { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
+    { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
+  ],
+};
+
+// 状态 2：默认固定任务
+export const WithPinnedTasks = Template.bind({});
+WithPinnedTasks.args = {
+  // Shaping the stories through args composition.
+  // Inherited data coming from the Default story.
+  tasks: [
+    ...Default.args.tasks.slice(0, 5),
+    { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+  ],
+};
+
+// 状态 3：加载中
+export const Loading = Template.bind({});
+Loading.args = {
+  tasks: [],
+  loading: true,
+};
+
+// 状态 4：空数据
+export const Empty = Template.bind({});
+Empty.args = {
+  // Shaping the stories through args composition.
+  // Inherited data coming from the Loading story.
+  ...Loading.args,
+  loading: false,
+};
