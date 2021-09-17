cb20cf4a0f1c3c53620172b59d9da6840ae08e11
蒋勇 <jiangy@highzap.com>
2021-09-17 13:59:28 +0800

    增加表示型组件、容器型组件文档描述

--- src/components/PureInboxScreen.vue
+++ src/components/PureInboxScreen.vue
@@ -1,5 +1,5 @@
 <template>
-<!-- 表示型组件 -->
+<!-- 表示型组件 https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0 -->
   <div>
     <div v-if="error" class="page lists-show">
       <div class="wrapper-message">
